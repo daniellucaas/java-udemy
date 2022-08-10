1eeaeafa7d6e9346570701c8a08be20d8223cad4
Daniel Lucas <dlucassm@gmail.com>
2022-08-10 16:55:13 -0300

    Tipos de dados: int

---
+++ src/dataTypes/DataTypes.java
@@ -0,0 +1,22 @@
+package dataTypes;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+
+		//Creating integer Variables
+		int a = 10;
+		short b = 99;
+		long c = 10012939009l;
+		byte d = 1;
+		
+		//Printing variable
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("c = " + c);
+		System.out.println("d = " + d);
+		
+		//Creating double variables
+	}
+
+}
