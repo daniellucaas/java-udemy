3e5573a3961a488549fa4d4d2d7e06f618eb628d
Daniel Lucas <dlucassm@gmail.com>
2022-08-11 07:06:45 -0300

    Aula 4

--- src/dataTypes/DataTypes.java
+++ src/dataTypes/DataTypes.java
@@ -9,14 +9,26 @@ public class DataTypes {
 		short b = 99;
 		long c = 10012939009l;
 		byte d = 1;
-		
-		//Printing variable
-		System.out.println("a = " + a);
-		System.out.println("b = " + b);
-		System.out.println("c = " + c);
-		System.out.println("d = " + d);
+		System.out.println("int = " + a);
+		System.out.println("short = " + b);
+		System.out.println("long = " + c);
+		System.out.println("byte = " + d);
 		
 		//Creating double variables
+		float e = 13.6654f;
+		double f = 16.2345234523452345d;
+		System.out.println("float = " + e);
+		System.out.println("double = " + f);
+		
+		//Creating char variables
+		char g = 'j';		
+		System.out.println("char = " + g);
+		
+		//Creating boolean variables
+		boolean h = true;
+		System.out.println("boolean = " + h);
+		
+		
 	}
 
 }
